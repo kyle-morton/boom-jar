8e07ef1ff466b39fdd198c3caeec5155a07684dd
Kyle Morton <kylemortondevelopment@gmail.com>
2022-10-09 15:31:03 -0500

    init

---
+++ App/Shared/BoomJarApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoomJarApp.swift
+//  Shared
+//
+//  Created by Kyle Morton on 10/9/22.
+//
+
+import SwiftUI
+
+@main
+struct BoomJarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
